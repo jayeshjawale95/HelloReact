bea342ce6a27f22364735d81c42f90ef7c12ef3e
jayesh <jawalejayesh1995@gmail.com>
2017-11-15 20:51:49 +0530

    React props

--- public/app.jsx
+++ public/app.jsx
@@ -1,17 +1,31 @@
 // Greeter component
 var Greeter = React.createClass({
+  // Default props
+  getDefaultProps: function () {
+    return {
+      name: "React",
+      message: "This is the default message!"
+    };
+  },
+
   render: function () {
+    var name = this.props.name; //props
+    var message = this.props.message;
+
     return (
       <div>
-        <h1>Hello React</h1>
-        <p>This is from a component</p>
+        <h1>Hello {name}</h1>
+        <p>{message +'!!'}</p>
       </div>
     );
   }
 });
 
+var firstName = "Jayesh";
+var message = "Message from Prop"
+
 // Render the dom elements or components
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName} message={message}/>,
   document.getElementById('app')
 );
