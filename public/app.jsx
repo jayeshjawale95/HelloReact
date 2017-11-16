4ed2802edc460a2b4a19ff21b77a41b8b9c2d48e
jayesh <jawalejayesh1995@gmail.com>
2017-11-16 11:17:43 +0530

    nested components

--- public/app.jsx
+++ public/app.jsx
@@ -38,16 +38,43 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}</h1>
         <p>{message +'!!'}</p>
-
+        <GreeterMessage/>
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button>Set Name</button>
         </form>
-
+        <GreeterForm/>
       </div>
     );
   }
 });
+
+// GreeterMessage component
+var GreeterMessage = React.createClass({
+  render: function () {
+      return (
+        <div>
+          <h1>Some H1</h1>
+          <p>Some Paragraph</p>
+        </div>
+      );
+  }
+});
+
+// Greeter form
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
+
+
 
 var firstName = "Jayesh";
 var message = "Message from Prop"
